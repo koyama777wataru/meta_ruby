0dd838e2876b6490c8f64eba3d755f1078108598
koyamawataru <koyama777wataru@gmail.com>
2014-04-16 10:28:32 +0900

    method_missing
    method_missing

--- meta_ruby2_3_1.rb
+++ meta_ruby2_3_1.rb
@@ -1,12 +1,53 @@
+#coding: utf-8
 require 'ruport'
-
 table = Ruport::Data::Table.new :column_names => ["country", "wine"],
                                 :data => [["France", "Bordeaux"],
                                           ["Italy", "Chianti"],
                                           ["France", "Chablis"]]
 puts table.to_text
-
+#動的表現で気にする事は下記のcountryである。
+#作者は私がcountryという文言を使う事を想定していたのであろうか？？
 found = table.rows_with_country("France")
 found.each do |row|
   puts row.to_csv
 end
+
+class MyOpenStruct
+  def initialize
+    @attributes = {}
+  end
+
+  def method_missing(name, *args)
+    #nameには”flavor=”が入る
+    attribute = name.to_s
+    if attribute =~ /=$/
+      @attributes[attribute.chop] = args[0]
+    else
+      @attribute[attribute]
+    end
+  end
+end
+
+o = MyOpenStruct.new
+o.flavor = "バニラ"
+
+#複数引数の確認
+#def test_args(*args)
+#  args.each do |val|
+#    p val
+#  end
+#end
+#test_args("aa", "bb")
+
+#正規表現確認
+#行末マッチ
+#p "test=" =~ /=$/
+
+#chop確認
+#最終文字をなくす
+#p "aaabb".chop
+
+#ハッシュ確認
+# hash = {}
+# hash["flavor"] = "hashtest"
+# p hash[:flavor]
